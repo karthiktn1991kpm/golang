a97ea32a72b4317c005f246733d0296838946acc
karthiktn1991kpm <karthiktn1991kpm@gmail.com>
2025-05-14 13:47:15 +0530

    Add files via upload

---
+++ main.go
@@ -0,0 +1,15 @@
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Hello, World!",
+		})
+	})
+
+	r.Run(":8080") // Start server on port 8080
+}
