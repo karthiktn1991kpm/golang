4a023f445cc03f1a1efd4cc9e99047410b3abce9
karthiktn1991kpm <karthiktn1991kpm@gmail.com>
2025-05-14 15:48:29 +0530

    Add files via upload

--- main.go
+++ main.go
@@ -1,17 +1,30 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
+// db variable creation
+
 func main() {
-	r := gin.Default()
+	router := gin.Default()
+	// Set up session store
+	router.Static("/static", "./static")
+	router.LoadHTMLGlob("templates/*.html")
+	//login page
+	router.GET("", loginpage)
+	router.GET("/index", index)
+	router.GET("/firstpage", firstpage)
+	router.Run(":8080")
 
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "Hello, World!",
-		})
-	})
+}
 
-	r.Run(":8080") // Start server on port 8080
+// AuthMiddleware validates session and user authentication
+func loginpage(c *gin.Context) {
+	c.HTML(http.StatusOK, "login.html", gin.H{})
+}
+func index(c *gin.Context) {
+	c.HTML(http.StatusOK, "index.html", gin.H{})
 }
